91355b8d8d5960096cefe0f5d3f279b1133438ea
Your Name <jaheimadaniels@gmail.com>
2026-02-28 18:16:54 -0500

    Implemented the booking modal and added browse.js

---
+++ frontend/client/browse.js
@@ -0,0 +1,7 @@
+
+  // Set service and price dynamically
+  function setService(name, price) {
+    document.getElementById('serviceName').innerText = name;
+    document.getElementById('servicePrice').innerText = price;
+  }
+
